0748bdf5175ff871f0dc67ddbf33b76cc13ae69a
Ekpenyong <okpo.ekpenyong@gmail.com>
2019-12-10 03:26:36 +0100

    created heroku db

--- dbinit.sql
+++ dbinit.sql
@@ -3,11 +3,9 @@ CREATE TABLE books (
   ID SERIAL PRIMARY KEY,
   author VARCHAR(255) NOT NULL,
   title VARCHAR(255) NOT NULL
-
 );
 
+INSERT INTO books (author, title)
+VALUES ('Clive James', 'Unreliable Memoirs'),('Elsa Pataky', 'Strong');
 
-INSERT INTO books (author, title)VALUES
-('Clive James', 'Unreliable Memoirs'),
-('Elsa Pataky', 'Strong');
-
+ 
