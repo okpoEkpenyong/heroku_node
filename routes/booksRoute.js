ad980a89fbc7bc3893c612e7321d8492c324a86a
Ekpenyong <okpo.ekpenyong@gmail.com>
2019-12-10 03:47:10 +0100

    edit booksRoute

--- routes/booksRoute.js
+++ routes/booksRoute.js
@@ -5,7 +5,7 @@ const db = require('../controllers/booksController');
 
 router.get('/', (req, res) => {
 
-   res.send(JSON.stringify({Hello: 'My Books World!'}))
+   res.send(JSON.stringify({Hello: 'Book People!'+ '...append /api/v1/books to this uri to see current books. Enjoy!'}))
 });
 
 router.get('/api/v1/books', db.getAllBooks);
